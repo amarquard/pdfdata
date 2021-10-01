b46f37e6a69bed9c0b23d97200e8e548f6264882
Andrea Marion Marquard <andrea@clinical-microbiomics.com>
2021-10-01 20:38:38 +0200

    Make package pass check

--- R/extract_pdf_table.R
+++ R/extract_pdf_table.R
@@ -6,32 +6,40 @@
 # install.packages("pdftools")
 # install.packages("tidyverse")
 
-for (x in c("tesseract", "pdftools", "tidyverse")) {
-  if (!require(x, character.only = TRUE)) {
-    install.packages(x, dependencies = TRUE)
-  }
-}
+# for (x in c("tesseract", "pdftools", "tidyverse")) {
+#   if (!require(x, character.only = TRUE)) {
+#     install.packages(x, dependencies = TRUE)
+#   }
+# }
 
-library(pdftools)
-library(tidyverse)
 
-# Function that takes one filepath, reads the text, and converts to a table
+#' Function that takes one filepath, reads the text, and converts to a table
+#'
+#' @param file path to pdf file
+#'
+#' @return tibble with data values from table
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' facs_to_table(file_path)
+#' }
 facs_to_table <- function(file) {
 
   # Read all content as one long character string
   raw_text <- pdftools::pdf_text(file)
 
   # Split it by newlines
-  raw_lines <- raw_text %>% str_split("\n")
+  raw_lines <- raw_text %>% stringr::str_split("\n")
 
   # Decide where to start and end
   start_pos <- grep("Parameter", raw_lines[[1]])
   end_pos <- grep("QC Messages", raw_lines[[1]])
 
   relevant_lines <- raw_lines[[1]][start_pos:(end_pos - 1)]
-  relevant_fields <- relevant_lines %>% str_split(" {2,}")
+  relevant_fields <- relevant_lines %>% stringr::str_split(" {2,}")
 
-  relevant_3_fields <- map(relevant_fields, ~{
+  relevant_3_fields <- purrr::map(relevant_fields, ~{
     if (length(.x)==2) {
       c(.x[1], "", .x[2]) # add the empty missing field
     } else {
@@ -41,35 +49,52 @@ facs_to_table <- function(file) {
 
   headers <- relevant_3_fields[[1]]
 
-  final_table <- map(relevant_3_fields[-1], ~set_names(x = .x, nm = headers)) %>%
-    bind_rows %>%
-    mutate(Percent = as.numeric(Percent),
+  final_table <- purrr::map(relevant_3_fields[-1], ~purrr::set_names(x = .x, nm = headers)) %>%
+    dplyr::bind_rows %>%
+    dplyr::mutate(Percent = as.numeric(Percent),
            `Value/AbsCnt` = as.numeric(`Value/AbsCnt`))
 
   return(final_table)
 }
 
-# Get a list of all files in the 'pdfs' folder
-files <- list.files("pdfs", pattern = "\\.pdf$", full.names = TRUE)
-names(files) <- basename(files)
 
-# Loop over each filename, and read the table
-pdf_tables <- imap(files, ~{
+#' Read data table from a pdf
+#'
+#' @param file_path path to pdf
+#'
+#' @return a tibble with extracted data values, and columns 'patient', 'sample' and 'file'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Get a list of all files in the 'pdfs' folder
+#' files <- list.files("pdfs", pattern = "\\.pdf$", full.names = TRUE)
+#'
+#' pdf_tables <- purrr::map(files, extract_pdf_table) %>%
+#'   # Combine the rows from each file
+#'   dplyr::bind_rows() %>%
+#'   # Get patient, sample and filename as the first columns
+#'   dplyr::select(patient, sample, file, dplyr::everything())
+#'
+#' pdf_tables
+#' }
+extract_pdf_table <- function(file_path) {
+
+  file_name <- basename(file_path)
+
   # Extract the patient name, which is XXXXXX. followed by a number of digits
-  patient <- str_extract(.y, "XXXXXX\\.\\d+")
+  patient <- stringr::str_extract(file_name, "XXXXXX\\.\\d+")
+
   # Extract the sample name, which is everything excepth the patient name and the .pdf suffix
-  sample <- str_remove(.y, "XXXXXX\\.\\d+_") %>% str_remove("\\.pdf")
+  sample <- stringr::str_remove(file_name, "XXXXXX\\.\\d+_") %>% stringr::str_remove("\\.pdf")
+
   # Now apply our facs_to_table function, and reshape to have one value per column
-  facs_to_table(.x) %>%
-    pivot_wider(names_from = Parameter, values_from = c(Percent, `Value/AbsCnt`)) %>%
-    bind_cols(patient = patient, sample = sample, file = .y)
-}) %>%
-  # Combine the rows from each file
-  bind_rows %>%
-  # Get patient, sample and filename as the first columns
-  select(patient, sample, file, everything())
+  facs_to_table(file_path) %>%
+    tidyr::pivot_wider(names_from = Parameter, values_from = c(Percent, `Value/AbsCnt`)) %>%
+    dplyr::bind_cols(patient = patient, sample = sample, file = file_name)
+}
 
-pdf_tables
+utils::globalVariables(c("Parameter", "Percent", "Value/AbsCnt"))
 
-# Save as excel, name the file by today's date
-openxlsx::write.xlsx(pdf_tables, file = paste0("excel/facs_tables_", Sys.Date(), ".xlsx"))
+# # Save as excel, name the file by today's date
+# openxlsx::write.xlsx(pdf_tables, file = paste0("excel/facs_tables_", Sys.Date(), ".xlsx"))
